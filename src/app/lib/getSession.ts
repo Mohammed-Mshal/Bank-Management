349e8387474f568fe0733ca02eddfce95618bfaf
Mohammed Mshal <mohammedmshal02@gmail.com>
2024-09-20 11:48:04 +0300

    Fix DB Connection

--- src/app/lib/getSession.ts
+++ src/app/lib/getSession.ts
@@ -1,11 +1,12 @@
 import { auth } from "@/auth"
+import { cache } from "react";
 
-export async function getSession () {
+export const getSession = cache(async () => {
     try {
         const session = auth()
         return session
     } catch (error) {
         console.log(error);
-        
+
     }
-}
+})
