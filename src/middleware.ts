eef3c59986ac1d4bfa50225b8ccfd84421779d6d
Mohammed Mshal <mohammedmshal02@gmail.com>
2024-09-24 19:44:04 +0300

    fix routning

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,5 @@
-import { type NextRequest, NextResponse } from "next/server"
-import { verifySession } from "./app/libs/session"
+import { NextRequest } from "next/server"
+import { updateCookies } from "./app/libs/session"
 export async function middleware(request: NextRequest) {
-  
-}
-
-
-export const config = {
-    matcher: [
-        '/',
-        '/:path*'
-    ],
+    return await updateCookies(request)
 }
