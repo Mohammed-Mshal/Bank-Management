b6c0b55da8c34a55332fcd82393c4e38717259c0
Mohammed Mshal <mohammedmshal02@gmail.com>
2024-09-24 20:00:59 +0300

    fix routning

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { verifySession } from '../libs/session';
+import { redirect } from 'next/navigation';
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const session = await verifySession();
+  if (!session?.userId) {
+    redirect("/auth/login");
+  }
   return (
     <div>Dashboard</div>
   )
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,5 @@
-import { type NextRequest, NextResponse } from "next/server"
-import { verifySession } from "./app/libs/session"
+import { NextRequest } from "next/server"
+import { updateCookies } from "./app/libs/session"
 export async function middleware(request: NextRequest) {
-    const authRoutes = '/auth'
-    const protectedRoute = ['/dashboard']
-    const currentPath = request.nextUrl.pathname
-    const isProtectedRoute = protectedRoute.includes(currentPath)
-    if (isProtectedRoute && !currentPath.startsWith(authRoutes)) {
-        const session = await verifySession();
-        if (!session?.userId) {
-            return NextResponse.redirect(new URL('/auth/login', request.nextUrl))
-        }
-    }
-    return NextResponse.next()
-}
-
-
-export const config = {
-    matcher: [
-        '/',
-        '/:path*'
-    ],
+    return await updateCookies(request)
 }
