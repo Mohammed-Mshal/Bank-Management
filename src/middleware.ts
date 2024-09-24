f354ad993bc1a76e3e041a131ee831745293bc2e
Mohammed Mshal <mohammedmshal02@gmail.com>
2024-09-24 20:07:51 +0300

    fix routning

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,14 @@
+import { cookies } from "next/headers"
 import { type NextRequest, NextResponse } from "next/server"
-import { verifySession } from "./app/libs/session"
+import { decrypt } from "./app/libs/session"
 export async function middleware(request: NextRequest) {
+    const authRoutes = '/auth'
     const protectedRoute = ['/dashboard']
     const currentPath = request.nextUrl.pathname
     const isProtectedRoute = protectedRoute.includes(currentPath)
-    if (isProtectedRoute) {
-        const session = await verifySession();
+    if (isProtectedRoute && !currentPath.startsWith(authRoutes)) {
+        const cookie = cookies().get('session')?.value
+        const session = await decrypt(cookie)
         if (!session) {
             return NextResponse.redirect(new URL('/auth/login', request.nextUrl))
         }
