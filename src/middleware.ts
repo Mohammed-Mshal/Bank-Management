9c5b316e47244cec1ce63eb246241609d76d3f3c
Mohammed Mshal <mohammedmshal02@gmail.com>
2024-09-24 20:20:52 +0300

    fix routning

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,43 @@ import { cookies } from "next/headers"
 import { type NextRequest, NextResponse } from "next/server"
 import { decrypt } from "./app/libs/session"
 export async function middleware(request: NextRequest) {
+    const authRoutes = '/auth'
+    const protectedRoute = ['/dashboard']
+    const currentPath = request.nextUrl.pathname
+    const isProtectedRoute = protectedRoute.includes(currentPath)
+    if (isProtectedRoute && !currentPath.startsWith(authRoutes)) {
+        const cookie = cookies().get('session')?.value
+        const session = await decrypt(cookie)
+        if (!session?.userId) {
+            return NextResponse.redirect(new URL('/auth/login', request.nextUrl))
+        }
+        else {
+            return NextResponse.next()
 
+        }
+    }
+    if (currentPath.startsWith(authRoutes)) {
+        const cookie = cookies().get('session')?.value
+        const session = await decrypt(cookie)
+        if (session?.userId) {
+            return NextResponse.redirect(new URL('/dashboard', request.nextUrl))
+        }
+        else {
+            return NextResponse.next()
+
+        }
+    }
+    if (currentPath === '/') {
+        const cookie = cookies().get('session')?.value
+        const session = await decrypt(cookie)
+        if (session?.userId) {
+            return NextResponse.redirect(new URL('/dashboard', request.nextUrl))
+        }
+        else {
+            return NextResponse.redirect(new URL('/auth/login', request.nextUrl))
+
+        }
+    }
     return NextResponse.next()
 }
 
